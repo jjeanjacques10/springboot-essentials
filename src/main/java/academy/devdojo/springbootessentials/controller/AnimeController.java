52529379dfcf0e2569cbb288113b16f70bf4a914
jjeanjacques10 <jjean.jacques10@gmail.com>
2022-04-05 08:18:58 -0300

    Video 7-9 - CRUD

--- src/main/java/academy/devdojo/springbootessentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springbootessentials/controller/AnimeController.java
@@ -2,16 +2,12 @@ package academy.devdojo.springbootessentials.controller;
 
 import academy.devdojo.springbootessentials.domain.Anime;
 import academy.devdojo.springbootessentials.repository.AnimeRepository;
-import academy.devdojo.springbootessentials.util.DataUtil;
+import academy.devdojo.springbootessentials.util.Utils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,22 +18,35 @@ import java.util.List;
 @RequestMapping("animes")
 public class AnimeController {
 
-    private final DataUtil dataUtil;
+    private final Utils utils;
     private final AnimeRepository animeRepository;
 
     @GetMapping
     public ResponseEntity<List<Anime>> listAll() {
-        log.info("Date formated: {}", dataUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        log.info("Date formated: {}", utils.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeRepository.listAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Anime> findById(@PathVariable int id) {
-        var animeFound = animeRepository.listAll()
-                .stream()
-                .filter(anime -> anime.getId() == id)
-                .findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime Not Found"));
-        return ResponseEntity.ok(animeFound);
+        return ResponseEntity.ok(animeRepository.findById(id));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
+        return ResponseEntity.ok(animeRepository.save(anime));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        animeRepository.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> update(@RequestBody Anime anime) {
+        animeRepository.update(anime);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
---
+++ src/main/java/academy/devdojo/springbootessentials/util/Utils.java
@@ -0,0 +1,25 @@
+package academy.devdojo.springbootessentials.util;
+
+import academy.devdojo.springbootessentials.domain.Anime;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Component
+public class Utils {
+
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime) {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
+
+    public Anime findAnimeOrThrowNotFound(int id, List<Anime> animes) {
+        return animes.stream()
+                .filter(anime -> anime.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime Not Found"));
+    }
+}
