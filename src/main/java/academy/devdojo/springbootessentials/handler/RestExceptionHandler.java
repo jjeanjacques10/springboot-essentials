8259bf44cf4ea5f36e1460c60bb1441e24f40aea
jjeanjacques10 <jjean.jacques10@gmail.com>
2022-04-06 13:19:16 -0300

    Video 18 - Overriding handleExceptionInternal

--- src/main/java/academy/devdojo/springbootessentials/handler/RestExceptionHandler.java
+++ src/main/java/academy/devdojo/springbootessentials/handler/RestExceptionHandler.java
@@ -1,15 +1,20 @@
 package academy.devdojo.springbootessentials.handler;
 
+import academy.devdojo.springbootessentials.exception.ExceptionDetails;
 import academy.devdojo.springbootessentials.exception.ResourceNotFoundDetails;
 import academy.devdojo.springbootessentials.exception.ResourceNotFoundException;
 import academy.devdojo.springbootessentials.exception.ValidationExceptionDetails;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,7 +22,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ResourceNotFoundDetails> handleResourceNotFoundException(ResourceNotFoundException exception) {
@@ -31,9 +36,9 @@ public class RestExceptionHandler {
                         .build(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
 
         String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
         String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
@@ -42,11 +47,23 @@ public class RestExceptionHandler {
                 ValidationExceptionDetails.builder()
                         .timestamp(LocalDateTime.now())
                         .status(HttpStatus.BAD_REQUEST.value())
-                        .title("Invalid fields")
+                        .title("Invalid Fields Error")
+                        .details(ex.getMessage())
                         .fields(fields)
                         .fieldsMessage(fieldsMessage)
-                        .details(exception.getMessage())
-                        .developerMethod(exception.getClass().getName())
+                        .developerMethod(ex.getClass().getName())
                         .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .title(ex.getCause().getMessage())
+                .details(ex.getMessage())
+                .developerMethod(ex.getClass().getName())
+                .build();
+        return new ResponseEntity(exceptionDetails, headers, status);
     }
 }
